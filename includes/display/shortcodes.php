4917e9e8539b7dfb36e099b9d92d0361db3feef2
Kyle B. Johnson <kyle@wpninjas.com>
2016-01-17 22:05:24 -0500

    add example calendar markup for shortcode.

--- includes/display/shortcodes.php
+++ includes/display/shortcodes.php
@@ -4,13 +4,17 @@ final class OCWP_Display_Shortcodes
 {
     public function __construct()
     {
+        wp_register_style( 'open_calendar_wp', OpenCalendarWP::get_url() . 'assets/css/style.css' );
+
         add_shortcode( 'open_calendar',     array( $this, 'display_calendar' ) );
         add_shortcode( 'open_calendar_wp',  array( $this, 'display_calendar' ) );
     }
 
     public function display_calendar( $atts = array() )
     {
+        wp_enqueue_style( 'open_calendar_wp' );
         echo "[OPEN CALEDAR HERE]";
+        echo OpenCalendarWP::render( 'display-calendar.html.php' );
     }
 
 }
